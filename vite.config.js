fdd32abbf0afdfc29d45ecf5f49dc000224299ce
kishorevarri <kishorekumarvarri@gmail.com>
2025-05-09 07:49:30 +0530

    update allowedhosts in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
       server: {
         host: "0.0.0.0",    
         port: 8080,
-        allowedHosts: ['ln-rfp-ai-assistant-frontend-dev-1089873937305.us-central1.run.app'],
+        allowedHosts: ['ln-replica-151472627439.us-central1.run.app'],
       },
       preview: {
         port: 4173
